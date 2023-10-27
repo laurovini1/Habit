63bdf51da5743c522e9aa1fc2f41cec36b0ab4f6
laurovini1 <lauro.vinicius.oliveira@gmail.com>
2023-10-27 13:28:30 -0300

    Primeiro Commit

---
+++ server/src/server.ts
@@ -0,0 +1,25 @@
+import fastify from 'fastify'
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
+
+const app = fastify()
+const prisma = new PrismaClient()
+
+app.register(cors)
+
+/*  CRIANDO ROTAS
+    MÉTODO HTTP: Get, Post, Put, Patch, Delete
+
+*/
+
+app.get('/hello',async () => {
+    const habits = await prisma.habit.findMany()
+
+    return habits
+})
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log('HTTP Server running!')
+})
---
+++ web/src/App.tsx
@@ -0,0 +1,29 @@
+import './styles/global.css';
+import { Habit } from "./components/Habit"
+
+function App(){
+  return (
+    <div>
+      <Habit completed={3}/>
+      <Habit completed={3}/>
+      <Habit completed={3}/>
+      <Habit completed={3}/>
+      <Habit completed={3}/>
+      <Habit completed={3}/>
+      <Habit completed={3}/>
+      <Habit completed={3}/>
+      <Habit completed={3}/>
+      <Habit completed={3}/>
+      <Habit completed={3}/>
+      <Habit completed={3}/>
+      <Habit completed={3}/>
+      <Habit completed={3}/>
+      <Habit completed={3}/>
+      <Habit completed={3}/>
+      <Habit completed={3}/>
+    </div>
+    
+  )
+}
+   
+export default App
