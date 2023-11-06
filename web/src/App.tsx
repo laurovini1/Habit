f7ae06bdcc2522a80dec6ce20592b53a38fc5641
laurovini1 <lauro.vinicius.oliveira@gmail.com>
2023-11-06 13:25:02 -0300

    Modelagem inicial page home

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,14 @@
+import { Header } from './components/Header';
 import './styles/global.css';
-
+ 
 //import { Habit } from "./components/Habit"
 
 export function App(){
   return (
-    <div>
-      
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header/>
+      </div>
     </div>
-
-
-
   )
 }
