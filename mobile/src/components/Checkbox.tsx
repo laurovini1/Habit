c8c2ce3fe7f671d7d0f4239eca367e6d7ed082ae
laurovini1 <lauro.vinicius.oliveira@gmail.com>
2023-11-30 15:48:10 -0300

    Checkbox

---
+++ mobile/src/components/Checkbox.tsx
@@ -0,0 +1,30 @@
+import { View, TouchableOpacity } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import colors from 'tailwindcss/colors';
+
+interface Props {
+    checked?: boolean;
+}
+
+export function Checkbox({checked = false}: Props){
+    return(
+        <TouchableOpacity
+            activeOpacity={0.7}
+            className='flex-row mb-2 items-center'
+        >
+            {
+                checked
+                    ?
+                    <View className='h-8 w-8 bg-green-500 rounded-lg  items-center justify-center'>
+                        <Feather
+                            name="check"
+                            size={20}
+                            color={colors.white}
+                        />
+                    </View>
+                    :
+                    <View className="h-8 w-8 bg-zinc-900 rounded-lg" />
+            }
+            </TouchableOpacity>
+    );
+}
